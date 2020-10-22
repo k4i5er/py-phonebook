fa742c7a2523c5e6779938d91637ceb686715efe
k4i5er <kaiser@kaiser-hw01.localdomain>
2020-10-22 13:08:33 -0500

    Added record deletion function

--- py-phonebook.py
+++ py-phonebook.py
@@ -106,16 +106,6 @@ def disable_entrys():
 
 
 def show_records():
-    global i
-    global e_name
-    global e_lastname
-    global e_organization
-    global e_homephone
-    global e_cellphone
-    global e_workphone
-    global e_personal_email
-    global e_work_email
-
     enable_entrys()
     delete_entry_second_window()
     e_name.insert(0, addressbook[i]["name"])
@@ -134,135 +124,189 @@ def modify_record():
     btn_modify.configure(text='Guardar registro', command=save_record)
 
 
-def open_new_window():
-    global e_name
-    global e_lastname
-    global e_organization
-    global e_homephone
-    global e_workphone
-    global e_cellphone
-    global e_personal_email
-    global e_work_email
-    global newWindow
-    global btn_back
-    global btn_forward
-    global btn_modify
+def delete_record():
+    global i
+    addressbook.pop(i)
+    # addressbook[0]
+    # addressbook[1] <-- me posiciono aquí para eliminar
+    # i -> 1
+    # Doy click a Eliminar
+    # addressbook[0]
+    # i -> 0
+    if i > len(addressbook) - 1:
+        i = len(addressbook) - 1
 
-    save_data()
-    delete_entry_main_window()
-    newWindow = Toplevel(root)
-    newWindow.geometry('400x400')
-    newWindow.title('Consulta de registros - Agenda v.1.0')
-    frm_name = Frame(newWindow)
-    Label(frm_name, text="Nombre").pack(side=LEFT)
-    e_name = Entry(frm_name)
-    e_name.pack(side=LEFT, fill=X, expand=1)
-    frm_name.pack(fill=X)
-
-    frm_lastname = Frame(newWindow)
-    lbl_lastname = Label(frm_lastname, text="Apellidos")
-    lbl_lastname.pack(side=LEFT, expand=1, fill=X)
-    e_lastname = Entry(frm_lastname)
-    e_lastname.pack(side=LEFT, fill=X, expand=1)
-    frm_lastname.pack(fill=X)
-
-    frm_organization = Frame(newWindow)
-    txt_showorganization = StringVar()
-    txt_showorganization.set('Organización: ')
-    lbl_organization = Label(frm_organization, text=txt_showorganization.get())
-    lbl_organization.pack(side=LEFT)
-    e_organization = Entry(frm_organization)
-    e_organization.pack(side=LEFT, fill=X, expand=1)
-    frm_organization.pack(fill=X)
-
-    frm_phones = Frame(newWindow)
-    Label(frm_phones, text='Teléfonos').pack(side=LEFT, expand=1, fill=X)
-    frm_phones.pack(fill=X)
-
-    frm_homephone = Frame(newWindow)
-    txt_showhomephone = StringVar()
-    txt_showhomephone.set('Casa: ')
-    lbl_homephone = Label(frm_homephone, text=txt_showhomephone.get())
-    lbl_homephone.pack(side=LEFT)
-    e_homephone = Entry(frm_homephone)
-    e_homephone.pack(side=LEFT, fill=X, expand=1)
-    frm_homephone.pack(fill=X)
-
-    frm_cellphone = Frame(newWindow)
-    txt_showcellphone = StringVar()
-    txt_showcellphone.set('Celular: ')
-    lbl_cellphone = Label(frm_cellphone, text=txt_showcellphone.get())
-    lbl_cellphone.pack(side=LEFT)
-    e_cellphone = Entry(frm_cellphone)
-    e_cellphone.pack(side=LEFT, fill=X, expand=1)
-    frm_cellphone.pack(fill=X)
-
-    frm_workphone = Frame(newWindow)
-    txt_showworkphone = StringVar()
-    txt_showworkphone.set('Laboral: ')
-    lbl_workphone = Label(frm_workphone, text=txt_showworkphone.get())
-    lbl_workphone.pack(side=LEFT)
-    e_workphone = Entry(frm_workphone)
-    e_workphone.pack(side=LEFT, fill=X, expand=1)
-    frm_workphone.pack(fill=X)
-
-    frm_emails = Frame(newWindow)
-    Label(frm_emails, text='Correos electrónicos').pack(
-        side=LEFT, expand=1, fill=X)
-    frm_emails.pack(fill=X)
-
-    frm_personal_email = Frame(newWindow)
-    txt_show_personal_email = StringVar()
-    txt_show_personal_email.set('Personal: ')
-    lbl_personal_email = Label(
-        frm_personal_email, text=txt_show_personal_email.get())
-    lbl_personal_email.pack(side=LEFT)
-    e_personal_email = Entry(frm_personal_email)
-    e_personal_email.pack(side=LEFT, fill=X, expand=1)
-    frm_personal_email.pack(fill=X)
-
-    frm_work_email = Frame(newWindow)
-    txt_show_work_email = StringVar()
-    txt_show_work_email.set('Laboral: ')
-    lbl_work_email = Label(frm_work_email, text=txt_show_work_email.get())
-    lbl_work_email.pack(side=LEFT)
-    e_work_email = Entry(frm_work_email)
-    e_work_email.pack(side=LEFT, fill=X, expand=1)
-    frm_work_email.pack(fill=X)
-
-    show_records()
-
-    frm_navigation_buttons = Frame(newWindow)
-    btn_back = Button(frm_navigation_buttons,
-                      text='<', command=move_backward)
-    if i != 0:
-        btn_back.pack(side=LEFT)
+    if len(addressbook) > 0:
+        show_records()
     else:
+        newWindow.destroy()
+
+    if i == len(addressbook) - 1:
+        btn_forward.pack_forget()
+    if len(addressbook) == 1:
         btn_back.pack_forget()
 
-    btn_forward = Button(frm_navigation_buttons,
-                         text='>', command=move_forward)
-    if i < len(addressbook)-1:
-        btn_forward.pack(side=LEFT)
+
+def validate_entrys():
+    validate = True
+
+    if len(entry_name.get()) == 0:
+        lbl_name.configure(style='error.TLabel')
+        validate = False
     else:
-        btn_forward.pack_forget()
+        lbl_name.configure(style='lbl.TLabel')
 
-    frm_navigation_buttons.pack()
+    if len(entry_lastname.get()) == 0:
+        lbl_lastname.configure(style='error.TLabel')
+        validate = False
+    else:
+        lbl_lastname.configure(style='lbl.TLabel')
 
-    frm_back_button = Frame(newWindow)
-    btn_modify = Button(
-        frm_back_button, text='Modificar registro', command=modify_record)
-    btn_modify.pack()
-    Button(frm_back_button, text='Regresar...',
-           command=close_window).pack()
-    frm_back_button.pack(fill=X)
+    if len(entry_cellphone.get()) == 0:
+        lbl_cellphone.configure(style='error.TLabel')
+        validate = False
+    else:
+        lbl_cellphone.configure(style='lbl.TLabel')
+
+    if len(entry_personal_email.get()) == 0:
+        lbl_personal_email.configure(style='error.TLabel')
+        validate = False
+    else:
+        lbl_personal_email.configure(style='lbl.TLabel')
+
+    return validate
 
 
-def close_window():
-    global newWindow
-    global i
-    i = 0
-    newWindow.destroy()
+def open_new_window():
+    is_valid = validate_entrys()
+    if is_valid:
+        global e_name
+        global e_lastname
+        global e_organization
+        global e_homephone
+        global e_workphone
+        global e_cellphone
+        global e_personal_email
+        global e_work_email
+        global newWindow
+        global btn_back
+        global btn_forward
+        global btn_modify
+        global i
+
+        i = 0
+        save_data()
+        delete_entry_main_window()
+        newWindow = Toplevel(root)
+        newWindow.geometry('400x400')
+        newWindow.title('Consulta de registros - Agenda v.1.0')
+        frm_name = Frame(newWindow)
+        Label(frm_name, text="Nombre").pack(side=LEFT)
+        e_name = Entry(frm_name)
+        e_name.pack(side=LEFT, fill=X, expand=1)
+        frm_name.pack(fill=X)
+
+        frm_lastname = Frame(newWindow)
+        lbl_lastname = Label(frm_lastname, text="Apellidos")
+        lbl_lastname.pack(side=LEFT, expand=1, fill=X)
+        e_lastname = Entry(frm_lastname)
+        e_lastname.pack(side=LEFT, fill=X, expand=1)
+        frm_lastname.pack(fill=X)
+
+        frm_organization = Frame(newWindow)
+        txt_showorganization = StringVar()
+        txt_showorganization.set('Organización: ')
+        lbl_organization = Label(
+            frm_organization, text=txt_showorganization.get())
+        lbl_organization.pack(side=LEFT)
+        e_organization = Entry(frm_organization)
+        e_organization.pack(side=LEFT, fill=X, expand=1)
+        frm_organization.pack(fill=X)
+
+        frm_phones = Frame(newWindow)
+        Label(frm_phones, text='Teléfonos').pack(side=LEFT, expand=1, fill=X)
+        frm_phones.pack(fill=X)
+
+        frm_homephone = Frame(newWindow)
+        txt_showhomephone = StringVar()
+        txt_showhomephone.set('Casa: ')
+        lbl_homephone = Label(frm_homephone, text=txt_showhomephone.get())
+        lbl_homephone.pack(side=LEFT)
+        e_homephone = Entry(frm_homephone)
+        e_homephone.pack(side=LEFT, fill=X, expand=1)
+        frm_homephone.pack(fill=X)
+
+        frm_cellphone = Frame(newWindow)
+        txt_showcellphone = StringVar()
+        txt_showcellphone.set('Celular: ')
+        lbl_cellphone = Label(frm_cellphone, text=txt_showcellphone.get())
+        lbl_cellphone.pack(side=LEFT)
+        e_cellphone = Entry(frm_cellphone)
+        e_cellphone.pack(side=LEFT, fill=X, expand=1)
+        frm_cellphone.pack(fill=X)
+
+        frm_workphone = Frame(newWindow)
+        txt_showworkphone = StringVar()
+        txt_showworkphone.set('Laboral: ')
+        lbl_workphone = Label(frm_workphone, text=txt_showworkphone.get())
+        lbl_workphone.pack(side=LEFT)
+        e_workphone = Entry(frm_workphone)
+        e_workphone.pack(side=LEFT, fill=X, expand=1)
+        frm_workphone.pack(fill=X)
+
+        frm_emails = Frame(newWindow)
+        Label(frm_emails, text='Correos electrónicos').pack(
+            side=LEFT, expand=1, fill=X)
+        frm_emails.pack(fill=X)
+
+        frm_personal_email = Frame(newWindow)
+        txt_show_personal_email = StringVar()
+        txt_show_personal_email.set('Personal: ')
+        lbl_personal_email = Label(
+            frm_personal_email, text=txt_show_personal_email.get())
+        lbl_personal_email.pack(side=LEFT)
+        e_personal_email = Entry(frm_personal_email)
+        e_personal_email.pack(side=LEFT, fill=X, expand=1)
+        frm_personal_email.pack(fill=X)
+
+        frm_work_email = Frame(newWindow)
+        txt_show_work_email = StringVar()
+        txt_show_work_email.set('Laboral: ')
+        lbl_work_email = Label(frm_work_email, text=txt_show_work_email.get())
+        lbl_work_email.pack(side=LEFT)
+        e_work_email = Entry(frm_work_email)
+        e_work_email.pack(side=LEFT, fill=X, expand=1)
+        frm_work_email.pack(fill=X)
+
+        show_records()
+
+        frm_navigation_buttons = Frame(newWindow)
+        btn_back = Button(frm_navigation_buttons,
+                          text='<', command=move_backward)
+        if i != 0:
+            btn_back.pack(side=LEFT)
+        else:
+            btn_back.pack_forget()
+
+        btn_forward = Button(frm_navigation_buttons,
+                             text='>', command=move_forward)
+        if i < len(addressbook)-1:
+            btn_forward.pack(side=LEFT)
+        else:
+            btn_forward.pack_forget()
+
+        frm_navigation_buttons.pack()
+
+        frm_back_button = Frame(newWindow)
+        btn_modify = Button(
+            frm_back_button, text='Modificar registro', command=modify_record)
+        btn_modify.pack(side=LEFT, expand=1)
+        btn_delete = Button(frm_back_button, text='Eliminar',
+                            command=delete_record)
+        btn_delete.pack(side=LEFT, expand=1)
+        Button(frm_back_button, text='Regresar...',
+               command=newWindow.destroy).pack(side=LEFT, expand=1)
+        frm_back_button.pack(fill=X)
 
 
 def move_backward():
@@ -294,18 +338,20 @@ root.geometry('400x400')
 root.title('Agenda v.1.0')
 
 myStyle = Style()
-myStyle.configure('lbl.TLabel', width=10)
+myStyle.configure('lbl.TLabel', width=10, foreground='#0000ff')
+myStyle.configure('error.TLabel', width=10, foreground='red')
 myStyle.configure('TLabel', foreground='#0000ff')
 
 frm_name = Frame(root)
-Label(frm_name, text="Nombre", style='lbl.TLabel').pack(side=LEFT, padx=10)
+lbl_name = Label(frm_name, text="Nombre", style='lbl.TLabel')
+lbl_name.pack(side=LEFT, padx=10)
 entry_name = Entry(frm_name)
 entry_name.pack(side=LEFT, expand=1, fill=X, padx=10)
 frm_name.pack(fill=X, ipady=10)
 
 frm_lastname = Frame(root)
-Label(frm_lastname, text="Apellidos",
-      style='lbl.TLabel').pack(side=LEFT, padx=10)
+lbl_lastname = Label(frm_lastname, text="Apellidos", style='lbl.TLabel')
+lbl_lastname.pack(side=LEFT, padx=10)
 entry_lastname = Entry(frm_lastname)
 entry_lastname.pack(side=LEFT, expand=1, fill=X, padx=10)
 frm_lastname.pack(fill=X, ipady=10)
@@ -328,7 +374,8 @@ entry_homephone.pack(side=LEFT, expand=1, fill=X, padx=10)
 frm_homephone.pack(fill=X, ipady=10)
 
 frm_cellphone = Frame(root)
-Label(frm_cellphone, text="Celular", style='lbl.TLabel').pack(side=LEFT, padx=10)
+lbl_cellphone = Label(frm_cellphone, text="Celular", style='lbl.TLabel')
+lbl_cellphone.pack(side=LEFT, padx=10)
 entry_cellphone = Entry(frm_cellphone)
 entry_cellphone.pack(side=LEFT, expand=1, fill=X, padx=10)
 frm_cellphone.pack(fill=X, ipady=10)
@@ -344,8 +391,9 @@ Label(frm_emails, text="Correos electrónicos").pack(side=LEFT, padx=10)
 frm_emails.pack(fill=X, ipady=10)
 
 frm_personal_email = Frame(root)
-Label(frm_personal_email, text="Personal",
-      style='lbl.TLabel').pack(side=LEFT, padx=10)
+lbl_personal_email = Label(
+    frm_personal_email, text="Personal", style='lbl.TLabel')
+lbl_personal_email.pack(side=LEFT, padx=10)
 entry_personal_email = Entry(frm_personal_email)
 entry_personal_email.pack(side=LEFT, expand=1, fill=X, padx=10)
 frm_personal_email.pack(fill=X, ipady=10)
